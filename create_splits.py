54710913e07c2b6ae727bb3ac2ef6e1e9784118a
brucefie <93987782+brucefie@users.noreply.github.com>
2022-06-15 15:06:50 -0300

    Editing create_splits.py

--- create_splits.py
+++ create_splits.py
@@ -15,44 +15,37 @@ def split(data_dir):
     args:
         - data_dir [str]: data directory, /home/workspace/data/waymo
     """
-    dataset = os.listdir(data_dir + 'training_and_validation')
-    np.random.shuffle(dataset)
-    train_sets, val_sets = np.split(dataset, [int(0.8 * len(dataset))])
-    
-    for i, data in enumerate(dataset):
-        if i < int(0.75 * len(dataset)):
-            shutil.move(data_dir+'/training_and_validation/'+ data, data_dir + '/train')
+
     # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
     # You should move the files rather than copy because of space limitations in the workspace.
+    print("Path written by the user: {} ".format(data_dir))
     
-    val_sets = os.path.join(data_dir,'val')
-    if os.path.exists(val_sets) == False:
-        os.makedirs(val_sets)
+    from_path = data_dir + '/waymo/training_and_validation'
+    to_path = data_dir + '/waymo/'
+            
+    val_dir = os.path.join(from_path , to_path + 'val')
+    if os.path.exists(val_dir) == False:
+        os.makedirs(val_dir)
     
-    train_sets = os.path.join(data_dir,'train')
-    if os.path.exists(train_sets) == False:
-        os.makedirs(train_sets)
+    train_dir = os.path.join(from_path , to_path + 'train')
+    if os.path.exists(train_dir) == False:
+        os.makedirs(train_dir)
+   
+    dataset = [filename for filename in glob.glob(f'{to_path}/*.tfrecord')]
+    print("Number of files: {}".format(len(dataset)))
+    np.random.shuffle(dataset) #organize the list
+    
+    train_sets, val_sets = np.split(dataset, [int(0.8 * len(dataset))]) #split into train and val
+    
+    
+    for t in train_sets:
+        print(t)
+        shutil.move(t, train_dir)
         
-    #test_sets = os.path.join(data_dir,'test')
-    #if os.path.exists(test_sets) == False:
-    #   os.makedirs(test_sets)
-                          
-    
-    files = [filename for filename in glob.glob(f'{data_dir}/*.tfrecord')]
-    np.random.shuffle(files)
-    train,val = np.split(files, [int(0.8*len(files))])
-    print(train)
-    print(val)
-  
-    
-    for t in train:
-        shutil.move(t,train_sets)
+    for v in val_sets:
+        print(v)
+        shutil.move(v, val_dir)
         
-    for v in val:
-        shutil.move(v,val_sets)
-        
-    #for t in test:
-    #    shutil.move(t,test_sets)
                  
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
